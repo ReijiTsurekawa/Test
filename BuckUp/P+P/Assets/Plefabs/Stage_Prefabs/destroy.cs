b0e718ec8f81e1b8dbfae31c286eae0b9184b01d
Tsurekawa <reiji.judo@gmail.com>
2019-02-22 18:32:24 +0900

    ステージ合わせました

---
+++ BuckUp/P+P/Assets/Plefabs/Stage_Prefabs/destroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+    float dead = 3.0f;
+	void Start () {
+	}
+	void Update () {
+        dead = dead - Time.deltaTime;
+        if (dead <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+        if (GetComponent<flore>().num == 1)
+        {
+            float time = dead;
+            dead = time;
+        }
+	}
+    public void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.name == "player")
+        {
+            GetComponent<flore>().num=0;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BuckUp/P+P/Assets/Plefabs/Stage_Prefabs/zp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zp : MonoBehaviour
+{
+    int x;
+    int z;
+    int num = 0;
+    public GameObject flore;
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        x = (int)pos.x;
+        z = (int)pos.z;
+    }
+
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter(Collision col)
+
+    {
+
+        if (col.gameObject.name == "player")
+        {
+            if (num == 0)
+            {
+                Vector3 pos2 = new Vector3(x, 0, z + 600);
+                Instantiate(flore, pos2, Quaternion.identity);
+                num++;
+            }
+
+        }
+    }
+}
